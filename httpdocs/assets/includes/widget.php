57efa4396793f69b3d18951b7a2fcebb13caa3ec
Flor Guzman <fguzman@sequelmediagroup.com>
2014-07-07 12:25:02 -0400

    Asthetics changes phase 2

---
+++ httpdocs/assets/includes/widget.php
@@ -0,0 +1,54 @@
+<section id="" class="sidebar shadow-on-large-up">
+
+<script type='text/javascript'>
+
+var _CI = _CI || {};
+
+(function() {
+
+var script = document.createElement('script');
+
+ref = document.getElementsByTagName('script')[0];
+
+_CI.counter = (_CI.counter) ? _CI.counter + 1 : 1;
+
+document.write('<div id="_CI_widget_');
+
+document.write(_CI.counter+'"></div>');
+
+script.type = 'text/javascript';
+
+script.src = 'http://widget.crowdignite.com/widgets/32363?_ci_wid=_CI_widget_'+_CI.counter;
+
+script.async = true;
+
+ref.parentNode.insertBefore(script, ref);
+
+})(); </script>
+
+<style>   
+
+#_ci_widget_div_32363{width:auto;max-width:280px;}
+
+#_ci_widget_div_32363 table{height:auto;width:100%;border-spacing:0;margin:5px 0;}
+
+#_ci_widget_div_32363 table tr{clear:both;height:auto;width:100%;display:inline-block;margin-bottom:10px;border-bottom: 2px solid #232323;}
+
+#_ci_widget_div_32363 table tr:last-child{border:0;}
+
+#_ci_widget_div_32363 table tr td{display:block;float:left;width:100%!important;text-align:left;}
+
+#_ci_widget_div_32363 table tr td img{-webkit-transition:all .2s ease;transition:all .2s ease;height:auto;width:100%!important;float:left;display:block;}
+
+#_ci_widget_div_32363 table tr td:hover img{opacity:.8;}
+
+#_ci_widget_div_32363 table tr td>a{float:left;width:100%;}
+
+#_ci_widget_div_32363 table tr td>br{display:none;}
+
+#_ci_widget_div_32363 table tr td > div{clear:none!important;margin-top:4%!important;width:100%!important;padding-bottom: 12px;}
+
+#_ci_widget_div_32363 table tr td > div a{color:#515E6C!important;font-family:Oslo, 'Helvetica Neue', Helvetica, Helvetica, Arial, sans-serif;font-size:13px!important;line-height:19.5px;text-decoration:none;}
+
+</style>
+</section>

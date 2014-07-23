79e6e644f30609742a6bc5a91d2b2d73116c274b
Flor Guzman <fguzman@sequelmediagroup.com>
2014-07-23 14:13:15 -0400

    left files

---
+++ httpdocs/assets/includes/featured_article.php
@@ -0,0 +1,46 @@
+<?php 
+	
+	$date = date("M d, Y", strtotime($featuredArticle['creation_date']));
+
+	$linkToArticle = $config['this_url'].$featuredArticle['cat_dir_name'].'/'.$featuredArticle["article_seo_title"];
+	$linkToACategory = $config['this_url'].$featuredArticle['cat_dir_name'];
+	$linkToImage = $config['image_url'].'articlesites/'.$mpArticle->data['article_page_assets_directory'].'/large/'.$featuredArticle['article_id'].'_tall.jpg';
+	$linkToContributor = $config['this_url'].'contributors/'.$featuredArticle['contributor_seo_name'];
+
+	if ( $detect->isMobile() ) { ?>
+
+	<div class="columns mobile-12 no-padding" id="<?php echo 'article-'.$articleIndex;?>">
+				<a class="mobile-12 no-padding" href="<?php echo $linkToArticle; ?>">
+					<img src="<?php echo $linkToImage; ?>" alt='<?php echo $featuredArticle['article_title']?>'>
+				</a>
+				<div class="mobile-12 no-padding-mobile">
+					<p class="mobile-12 no-padding uppercase" >
+						<span class="span-category <?php echo $featuredArticle['cat_dir_name']?>"><a href="<?php echo $linkToACategory; ?>" ><?php echo $featuredArticle['cat_name']?></a></span>
+					</p>
+					<a class="left clear-left" href="<?php echo $linkToArticle; ?>">
+						<h1 class="no-margin-mobile"><?php echo $featuredArticle['article_title']?></h1>
+					</a>
+				</div>
+			</div>
+
+<?php } else {?>
+<div class="columns mobile-12 small-12 medium-12 large-12 xlarge-12 no-padding" id="article-featured-1">
+	<a class="mobile-12 small-12 medium-12 large-12 xlarge-12" href="<?php echo $linkToArticle; ?>">
+		<img src="<?php echo $linkToImage; ?>" alt='<?php echo $featuredArticle['article_title']?>'>
+	</a>
+	<div class="mobile-12 small-12 medium-12 large-12 xlarge-12 mobile-vertical-center padding-top">
+		<p class="left uppercase" >
+			<span class="span-category <?php echo $featuredArticle['cat_dir_name']?>"><a href="<?php echo $linkToACategory; ?>" ><?php echo $featuredArticle['cat_name']?></a></span>
+			<span class="span-date"><?php echo $date; ?></span>
+		</p>
+		<p class="right uppercase">
+			<span class="span-author">By <a href="<?php echo $linkToContributor; ?>" ><?php echo $featuredArticle['contributor_name']; ?></a></span>
+		</p>
+		<a class="left clear-left" href="<?php echo $linkToArticle; ?>">
+			<h1 class="h1-large-article"><?php echo $featuredArticle['article_title']?></h1>
+		</a>
+	</div>
+</div>
+
+<?php } ?>
+<hr class="padding-top">
---
+++ httpdocs/assets/includes/prevnextarticles.php
@@ -0,0 +1,77 @@
+<?php
+if(isset($articleInfoObj) && $articleInfoObj){
+	if( !isset($article_id) ) $article_id = $articleInfoObj['article_id'];
+	$cat_id = $category['cat_id'];;
+
+	$nextArticle = $mpArticle->getPrevArticle( $article_id, $cat_id );
+	$prevArticle = $mpArticle->getNextArticle( $article_id, $cat_id );
+	$borderClassContainer = 'add-border add-shadow';
+	$borderClass = 'remove-border';
+	$rightBorder = 'right-border';
+
+	if( !$nextArticle ){
+		$borderClassContainer = 'remove-border';
+		$borderClass = 'add-border add-shadow';
+		$rightBorder = '';
+	}
+	if( !$prevArticle ){
+		$borderClassContainer = 'remove-border';
+		$borderClass = 'add-border right add-shadow';
+	}
+	
+	?>
+	<div id="prev-next-articles" class="row  hide-for-print clear <?php echo $borderClassContainer; ?>">
+		<?php
+		if( isset($prevArticle) && $prevArticle){
+
+			$prev_article_img = $config['image_url'].'articlesites/puckermob/large/'.$prevArticle['article_id'].'_tall.jpg';;
+			$prev_article_title = $prevArticle['article_title'];
+			$prev_article_url = $config['this_url'].$prevArticle['cat_dir_name'].'/'.$prevArticle["article_seo_title"];
+			?>
+			<!-- PREV ARTICLE -->
+			<div id="prev-article" class="columns mobile-12 small-12 medium-6 <?php echo $borderClass.' '.$rightBorder; ?>">
+				<div class="columns small-5 half-padding">
+					<div class="direction previous">
+						<a href="<?php echo $prev_article_url; ?>">
+							<i class="fa fa-2x fa-caret-left left"></i>
+							<p class="left">Previous</p>
+						</a>
+					</div>
+					<p class="small-title align-right clear center-small-title"><a href="<?php echo $prev_article_url; ?>" class="prefetch"><?php echo $mpHelpers->truncate($prev_article_title, 60); ?></a></p>
+				</div>
+				<div class="columns small-7 half-padding">
+					<a href="<?php echo $prev_article_url; ?>"><img src="<?php echo $prev_article_img; ?>" alt="<?php echo $prev_article_title.' Image'; ?>" /></a>
+				</div>
+			</div>
+			<?php } ?>
+
+
+			<?php
+			if( isset($nextArticle) && $nextArticle){
+
+				$next_article_img = $config['image_url'].'articlesites/puckermob/large/'.$nextArticle['article_id'].'_tall.jpg';;
+				$next_article_title = $nextArticle['article_title'];
+				$next_article_url = $config['this_url'].$nextArticle['cat_dir_name'].'/'.$nextArticle["article_seo_title"];
+				?>
+				<!-- NEXT ARTICLE -->
+				<div id="next-article" class="columns mobile-12 small-12 medium-6 <?php echo $borderClass; ?>">
+
+					<div class="columns small-7 half-padding">
+						<a href="<?php echo $next_article_url; ?>"><img src="<?php echo $next_article_img; ?>" alt="<?php echo $next_article_title.' Image'; ?>" /></a>
+					</div>
+					<div class="columns small-5 half-padding">
+						<div class="direction next">
+							<a href="<?php echo $next_article_url; ?>">
+								<i class="fa fa-2x fa-caret-right right"></i>
+								<p class="right">Next</p>
+							</a>
+						</div>
+						<p class="small-title align-lef clear center-small-title"><a href="<?php echo $next_article_url; ?>" class="prefetch"><?php echo $mpHelpers->truncate($next_article_title, 60); ?></a></p>
+					</div>
+
+				</div>
+				<?php } ?>
+
+			</div>
+
+			<?php } ?>

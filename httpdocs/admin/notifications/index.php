b8652a7a6f973a449ccda7d410e1bfe6e64ebdd8
Flor Guzman <florguzman@Flors-MacBook-Pro-7.local>
2014-12-04 21:30:17 -0500

    04/12/2014

---
+++ httpdocs/admin/notifications/index.php
@@ -0,0 +1,48 @@
+<?php 
+	$admin = true;
+	require_once('../../assets/php/config.php');
+	if(!$adminController->user->getLoginStatus()) $adminController->redirectTo('login/');
+
+	//Verify if is a content provider user
+	$content_provider = false;
+
+
+	$userData = $adminController->user->data = $adminController->user->getUserInfo();
+	$admin_user = false;
+	if(isset($adminController->user->data['user_type']) && $adminController->user->data['user_type'] == 1 || $adminController->user->data['user_type'] == 2){
+		$admin_user = true;
+	}
+
+	if(!$adminController->user->checkPermission('user_permission_show_add_notifications')) $adminController->redirectTo('noaccess/');
+
+	
+
+	if(isset($_POST['submit'])){
+		if($adminController->checkCSRF($_POST)){  //CSRF token check!!!
+			
+		}else $adminController->redirectTo('logout/');
+	}
+?>
+
+<!DOCTYPE html>
+
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<?php include_once($config['include_path_admin'].'head.php');?>
+<body>
+<?php include_once($config['include_path_admin'].'header.php');?>
+
+	<main id="main-cont" class="row panel sidebar-on-right" role="main">
+		<?php include_once($config['include_path_admin'].'menu.php');?>
+		
+		<div id="content" class="columns small-9 large-11">
+
+		</div>	
+	</main>
+	<?php include_once($config['include_path_admin'].'footer.php');?>
+
+	<?php include_once($config['include_path_admin'].'bottomscripts.php');?>
+</body>
+</html>
---
+++ httpdocs/assets/php/class.ManageAdminDashboard.php
@@ -0,0 +1,112 @@
+<?php
+require_once dirname(__FILE__).'/Connector.php';
+
+
+class ManageAdminDashboard{
+	
+	protected $config;
+	protected $con;
+
+
+	public function __construct($c){
+		$this->config = $c;
+		$this->con = new Connector($this->config);
+	}
+
+	protected function performQuery($opts){
+		$options = array_merge(array(
+			'queryString' => '',
+			'queryParams' => array(),
+			'returnRowAsSingleArray' => true,
+			'bypassCache' => false,
+				'returnCount' => false  //	true: performQuery will only return a count of rows
+		), $opts);
+		
+		$cachedData = false;
+		if($cachedData === false || $options['bypassCache'] === true){
+			$pdo = $this->con->openCon();
+			$q = $pdo->prepare($options['queryString']);
+			$q->execute($options['queryParams']);
+			if($q && $q->rowCount()){
+				$r = array();
+				while($row = $q->fetch(PDO::FETCH_ASSOC)){
+					$r[] = $row;
+				}
+			}else $r = false;
+			if($options['returnRowAsSingleArray'] === true && $r && count($r) == 1) $r = $r[0];
+			$this->con->closeCon();
+
+			if($options['returnCount'] === true){
+				return $q->rowCount();
+			}
+
+			return $r;
+		}else return $cachedData;
+	}
+
+	public function getWarningsMessages(){
+
+	}
+
+	public function saveWarningsMessages(){
+
+	}
+
+	public function getTopSharedMoblogs(){
+		$s = "SELECT 'flor', articles.article_id, articles.article_title, articles.article_seo_title, article_contributors.contributor_id,  article_contributors.contributor_name,  article_contributors.contributor_image,  social_media_records.date_updated, social_media_records.category, (SUM(facebook_shares) + SUM(twitter_shares) + SUM(pinterest_shares) + SUM(google_shares) + SUM(linkedin_shares) + SUM(delicious_shares) + SUM(stumbleupon_shares)) as 'total_shares'  
+			  FROM social_media_records 
+			   INNER JOIN ( articles, article_contributors, article_contributor_articles ) 
+				ON social_media_records.article_id = articles.article_id AND article_contributor_articles.article_id = articles.article_id AND article_contributor_articles.contributor_id = article_contributors.contributor_id 
+
+				WHERE articles.article_status = 1 
+				GROUP BY social_media_records.article_id
+				ORDER BY total_shares DESC LIMIT 5";
+
+		$q = $this->performQuery(['queryString' => $s]);
+
+		if ($q && isset($q[0])){
+				// If $q is an array of only one row (The set only contains one article), return it inside an array
+			return $q;
+		} else if ($q && !isset($q[0])){
+				// If $q is an array of rows, return it as normal
+			$q = array($q);
+			return $q;
+		} else {
+			return false;
+		}
+	}
+	
+
+	public function getAnnouncements(){
+
+	}
+
+	public function saveAnnouncements(){
+
+	}
+
+	public function getTrendingTopics(){
+
+	}
+
+	public function getRecentArticlesByContributor(){
+
+	}
+
+	public function bd_nice_number($n) {
+        // first strip any formatting;
+        $n = (0+str_replace(",","",$n));
+        
+        // is this a number?
+        if(!is_numeric($n)) return false;
+        
+        // now filter it;
+        if($n>1000000000000) return round(($n/1000000000000),1).'T';
+        else if($n>1000000000) return round(($n/1000000000),1).'B';
+        else if($n>1000000) return round(($n/1000000),1).'M';
+        else if($n>1000) return round(($n/1000),1).'K';
+        
+        return number_format($n);
+    }
+}
+?>

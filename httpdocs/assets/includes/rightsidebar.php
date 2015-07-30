70be7d711558a25e45b1bce7984f92a36e33dee3
palermod <palermo.deschamps@gmail.com>
2015-07-30 11:21:46 -0400

    took out stuff that was messing with sticky sidebar

--- httpdocs/assets/includes/rightsidebar.php
+++ httpdocs/assets/includes/rightsidebar.php
@@ -119,12 +119,14 @@
               </a>
               </noscript>
           </div>
-          <div>
-          <section id="sub-sidebar-2" class="sidebar sticky clear" style="padding-top:10px; clear:both;">
+          
+          <section id="sub-sidebar-2" class="sidebar sticky" style="padding-top:10px; clear:both;">
             <a href="https://www.surveymonkey.com/r/QMXVYTC"> <img src="https://s3.amazonaws.com/pucker-mob/images/midnight.png"></a>
             <a href="http://www.puckermob.com/admin/register/"> <img src="https://s3.amazonaws.com/pucker-mob/images/themob.png"></a>
           </section>
-          </div>
+    
+   
+        
           
         <?php }
       } 

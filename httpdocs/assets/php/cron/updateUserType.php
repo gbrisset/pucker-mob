b44a812fb3d97135881d9b94dadbc7136a596b6a
= <=>
2016-06-22 16:51:27 -0400

    June 22

--- httpdocs/assets/php/cron/updateUserType.php
+++ httpdocs/assets/php/cron/updateUserType.php
@@ -7,17 +7,16 @@
 	$user_id = $user->getUserId();
 	$user_name = $user->getUserName();
 	$user_type = $user->getUserType();
+	//$user_email= $user->getUserEmail();
+
 	$current_pageviews = $prev_pageviews = 0;
 	$contributor = $user->getContributorInfo();
 	$contributor_earnings = null;
 	$min_pv = 25000;
-	$max_pv = 50000;
-	
-	$post = [
-		'user_id' => '960',
-		'user_type' => 3
-	];
-
+	$max_pv = 35000;
+	$notification_obj = new Notification();
+	$notification = $notification_obj->getNotificationByUser($user_id);
+var_dump($notification_obj->getNotificationStatus());
 	if($contributor){
 		$contributor_earnings = $contributor->getContributorEarnings( $user->getContributorInfo(), 2 );
 
@@ -26,27 +25,40 @@
 
 		$current_pageviews = $current_month->total_us_pageviews;
 		$prev_pageviews = $prev_month->total_us_pageviews;
-
-
-		print_r($current_pageviews);
-		print_r("/n");
-		print_r($prev_pageviews);
-$prev_pageviews = 4000; $current_pageviews = 4000;
+		$current_pageviews = 5000;
+		$prev_pageviews = 5000;
+		
 		// Verify if Prev & Current are above the minimum pageviews per month.
 		//Upgrade to PRO BLOGGER [8]
 		if( $prev_pageviews >= $min_pv && $current_pageviews >=  $min_pv ){
 			//UPGRADE
-			echo "UP";
+			$data = [
+				"user_id" => $user_id,
+				"user_type" => 8
+			];
+
+			//Update User Type value
+			$user->setUserType( $data );
+			echo "UP & NOTIFY";
+
+			//var_dump($user->getUserType());
 		}
 
 		// Verify if Prev & Current are below the minimum pageviews per month
 		//Downgrade from PRO to BLOGGER [3]
 		if( $prev_pageviews < $min_pv && $current_pageviews < $min_pv ){
 			//DOWNGRADE
-			echo "DOWN";
-		}
+			echo "DOWN & NOTIFY";
+			$data = [
+				"user_id" => $user_id,
+				"user_type" => 3
+			];
 
-		if($prev_pageviews < $min_pv || $current_pageviews < $min_pv ){
+			//Update User Type value
+			$user->setUserType( $data );
+			
+
+		}elseif($prev_pageviews < $min_pv || $current_pageviews < $min_pv ){
 			//NOTIFY
 			echo "NOTIFY";
 		}

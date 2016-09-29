29e59133338895b37a6f1e4668c8cf8315ccdd46
= <=>
2016-09-29 18:51:39 -0400

    Thursday

--- httpdocs/assets/php/cron/updateUserType.php
+++ httpdocs/assets/php/cron/updateUserType.php
@@ -2,14 +2,19 @@
 	
 	$admin = true;
 	require '../config.php';
-
+	//	error_reporting(E_ALL);
+	//	ini_set('display_errors', '1');
 	$userObj = new User();
 	$notification_obj = new Notification(); 
+	
+	$user_list = $userObj->all("3, 8"); //Get all blogger Basic and Pro
 
-	//Get all blogger Basic and Pro
-	$user_list = $userObj->all("3, 8");
-	$current_month =  date('n') -1;
-	$prev_month = date('n') -2 ;
+//var_dump("TOTAL: ".count($user_list) );
+
+
+	//IF CURRENT MONTH IS OCT => $current_month value will be Sept and Prev_month value will be Aug.
+	$current_month =  date('n')-1;
+	$prev_month = date('n')-2 ;
 	$current_year = date('Y');
 	foreach($user_list as $user){
 		//Initialize Contributor Information
@@ -23,10 +28,9 @@
 		$limit = 25000;
 
 	
-		if($contributor){
-			//$contributor_earnings = $contributor->getContributorEarnings( $user->getContributorInfo(), 2 );
-var_dump("CONT_ID : $contributor->contributor_id : $prev_month, $current_month : $current_year ");
-
+		if($contributor && isset($contributor->contributor_id) ) {
+//var_dump("CONT_ID : $contributor->contributor_id : $prev_month, $current_month : $current_year ");
+		
 			$contributor_earnings = $contributor->getContributorEarningsPerMonth( $contributor, "$prev_month, $current_month", "$current_year" );
 			$current_month_obj = isset($contributor_earnings[0]) ? $contributor_earnings[0] : false;
 			$prev_month_obj = isset($contributor_earnings[1]) ? $contributor_earnings[1] : false;
@@ -34,35 +38,33 @@ var_dump("CONT_ID : $contributor->contributor_id : $prev_month, $current_month :
 			if(isset($current_month_obj) && $current_month_obj) $current_pageviews = $current_month_obj->total_us_pageviews;
 			if(isset($prev_month_obj) && $prev_month_obj) $prev_pageviews = $prev_month_obj->total_us_pageviews;
 			
-var_dump(" PREV: $prev_pageviews, CURRENT: $current_pageviews"); 
+//var_dump(" PREV: $prev_pageviews, CURRENT: $current_pageviews"); 
 
 			// Verify if Prev & Current are above the minimum pageviews per month.
 			//Upgrade to PRO BLOGGER [8]
 			if( $prev_pageviews >= $limit && $current_pageviews >=  $limit ){
-				//UPGRADE
-
 				if( $user_type == 3 ){
-var_dump("UPGRADE TO PRO: ", $user_name, $user_id,  "--------------------------------------");
+//var_dump("UPGRADE TO PRO: ", $user_name, $user_id,  "--------------------------------------");
 				
 					$data = [ "user_id" => $user_id, "user_type" => 8 ];
 					$not  = [ "user_id" => $user_id, "message" => "Upgrade to PRO BLOGGER", "type" => 2 , "date" => date( 'Y-m-d H:s:i', strtotime('now'))];
 
 					//Update User Type value
-					//$user->updateObj( $data );
+					$user->updateObj( $data );
 					
 					//Add a  Notification for this user
-					//$already_notified = $notification_obj->getByType(2, $user_id);
-					//if( count($already_notified) > 0 ){
-					//	$not['notification_id'] = $already_notified[0]->notification_id;
-						//$notification_obj->updateObj( $not );
-				//	}else{
-						//$notification_obj->saveObj( $not );
-					//}
+					$already_notified = $notification_obj->getByType(2, $user_id);
+					if( count($already_notified) > 0 ){
+						$not['notification_id'] = $already_notified[0]->notification_id;
+						$notification_obj->updateObj( $not );
+					}else{
+						$notification_obj->saveObj( $not );
+					}
 				}
 				
 			}elseif( $prev_pageviews < $limit && $current_pageviews < $limit ){
 				if( $user_type == 8 ||  $user_type == 9 ){
-var_dump("DOWNGRADE TO BASIC: ", $user_name, $user_id,  "--------------------------------------");
+//var_dump("DOWNGRADE TO BASIC: ", $user_name, $user_id,  "--------------------------------------");
 				
 					// Verify if Prev & Current are below the minimum pageviews per month
 					//Downgrade from PRO to BLOGGER [3]
@@ -70,26 +72,25 @@ var_dump("DOWNGRADE TO BASIC: ", $user_name, $user_id,  "-----------------------
 					$not  = [ "user_id" => $user_id, "message" => "Downgrade from PRO to BLOGGER", "type" => 2, "date" => date( 'Y-m-d H:s:i', strtotime('now')) ];
 
 					//Update User Type value
-					//$user->updateObj( $data );
+					$user->updateObj( $data );
 					
 					///Add a  Notification for this user
-				//	$already_notified = $notification_obj->getByType(2, $user_id);
-				//	if( count($already_notified) > 0 ){
-				//		$not['notification_id'] = $already_notified[0]->notification_id;
-					//	$notification_obj->updateObj( $not );
-				//	}else{
-					//	$notification_obj->saveObj( $not );
-				//	}
+					$already_notified = $notification_obj->getByType(2, $user_id);
+					if( count($already_notified) > 0 ){
+						$not['notification_id'] = $already_notified[0]->notification_id;
+						$notification_obj->updateObj( $not );
+					}else{
+						$notification_obj->saveObj( $not );
+					}
 				}
 			}
 
 			if($prev_pageviews >= $limit && $current_pageviews < $limit ){
 				//WARNING
 				if( $user_type == 8 ||  $user_type == 9 ){
-var_dump("WARNING: ", $user_name, $user_id,  "--------------------------------------");
-				
-				//	$not  = [ "user_id" => $user_id, "message" => "Warnings", "type" => 2 , "date" => date( 'Y-m-d H:s:i', strtotime('now'))];
-				//	$notification_obj->saveObj( $not );
+//var_dump("WARNING: ", $user_name, $user_id,  "--------------------------------------");
+					$not  = [ "user_id" => $user_id, "message" => "Warnings", "type" => 2 , "date" => date( 'Y-m-d H:s:i', strtotime('now'))];
+					$notification_obj->saveObj( $not );
 				}
 
 			}

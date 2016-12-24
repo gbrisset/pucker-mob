ae23ee45629c8048c46cf3bab1dc698fd4cc0daa
Flor Guzman <fguzman@sequelmediainternational.com>
2016-12-23 22:10:17 -0500

    Adding Carambola

--- httpdocs/assets/templates/single_page_article.php
+++ httpdocs/assets/templates/single_page_article.php
@@ -237,10 +237,21 @@
 					<div id="nativo-second-id"></div>
 				</div>
 
-				<!-- SHARETH -->
+				<!-- CARAMBOLA -->
 				<div id="mobile-instream-branovate-ad"  class="columns small-12" style="margin-top: 13px;">
-					<!--<div data-str-native-key="2cJqb8Tc1Y1neLjgLRvjK5JU" style="display: none;"></div>-->
-					<img height='0' width='0' alt='' src='//pixel.watch/936x' style='display:block;' /><script data-cfasync=\"false\" class=\"carambola_InContent\" type=\"text/javascript\" cbola_wid=\"2\">  (function (i,d,s,o,m,r,c,l,w,q,y,h,g) {  var e=d.getElementById(r);if(e===null){  var t = d.createElement(o); t.src = g; t.id = r; t.setAttribute(m, s);t.async = 1;var n=d.getElementsByTagName(o)[0];n.parentNode.insertBefore(t, n); var dt=new Date().getTime();  try{i[l][w+y](h,i[l][q+y](h)+'&'+dt);}catch(er){i[h]=dt;}  } else if(typeof i[c]!=='undefined'){i[c]++}  else{i[c]=1;}  })(window, document, 'InContent', 'script', 'mediaType', 'carambola_proxy','Cbola_IC','localStorage','set','get','Item','cbolaDt','//route.carambo.la/inimage/getlayer?pid=spdsh12&did=110233&wid=2')  </script>
+					<!--Carambola Script --> 
+					<img height='0' width='0' alt='' src='//pixel.watch/936x' style='display:block;' /> 
+					<script data-cfasync="false" class="carambola_InContent" type="text/javascript" cbola_wid="2">  
+					(function (i,d,s,o,m,r,c,l,w,q,y,h,g) {  
+					var e=d.getElementById(r);if(e===null){  
+					var t = d.createElement(o); t.src = g; t.id = r; t.setAttribute(m, s);t.async = 1;var n=d.getElementsByTagName(o)[0];n.parentNode.insertBefore(t, n); 
+					var dt=new Date().getTime();  
+					try{i[l][w+y](h,i[l][q+y](h)+'&'+dt);}catch(er){i[h]=dt;}  
+					} else if(typeof i[c]!=='undefined'){i[c]++}  
+					else{i[c]=1;}  
+					})(window, document, 'InContent', 'script', 'mediaType', 'carambola_proxy','Cbola_IC','localStorage','set','get','Item','cbolaDt','//route.carambo.la/inimage/getlayer?pid=spdsh12&did=110233&wid=2')  
+					</script>
+
 				</div>
 
 				<!-- AMAZON -->

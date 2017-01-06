16926b729394baf0f808ef908666bef2cb54ecbb
gbrisset <brissetconsulting@gmail.com>
2017-01-06 16:03:35 -0500

    Adding Adhesion Ad

--- httpdocs/assets/includes/bottomscripts.php
+++ httpdocs/assets/includes/bottomscripts.php
@@ -77,23 +77,20 @@ x
                // if ( $current_time->getTimestamp() > $start_time->getTimestamp() && $current_time->getTimestamp() < $end_time->getTimestamp()){ 
            
             ?>
-
-             <?php if( $articleInfoObj['article_id'] == 22227){?>
-            <!-- kixer adhesion -->
-            <!-- Start Pucker Mob - Adhesion - iframe buster code-->
-            <div id='__kx_ad_4915'></div>
-            <script type="text/javascript" language="javascript" id="__kx_tag_4915">
-            var __kx_ad_slots = __kx_ad_slots || [];
-
-            (function () {
-              var slot = 4915;
-              var h = false;
-              var doc = document;
-              __kx_ad_slots.push(slot);
-              if (typeof __kx_ad_start == 'function') {
-                __kx_ad_start();
-              } else {
-                if (top == self) {      var s = doc.createElement('script');
+              
+              <!-- Start Pucker Mob - Adhesion -->
+              <div id='__kx_ad_4915'></div>
+              <img id='__kx_ad_4915_px' src='%%VIEW_URL_UNESC%%http://cdn.kixer.com/img/transparent.gif?cb=%%CACHEBUSTER%%' />
+              <script type="text/javascript" language="javascript">
+              var __kx_ad_slots = __kx_ad_slots || [];
+              (function () {
+                var slot = 4915;
+                var h = false;
+                __kx_ad_slots.push(slot);
+                  if (typeof __kx_ad_start == 'function') {
+                  __kx_ad_start();
+                } else {
+                  var s = document.createElement('script');
                   s.type = 'text/javascript';
                   s.async = true;
                   s.src = '//cdn.kixer.com/ad/load.js';
@@ -104,20 +101,13 @@ x
                       __kx_ad_start();
                     }
                   };
-                  var x = doc.getElementsByTagName('script')[0];
+                  var x = document.getElementsByTagName('script')[0];
                   x.parentNode.insertBefore(s, x);
-                } else {        var tag = doc.getElementById('__kx_tag_'+slot);       var win = window.parent;
-                  doc = win.document;       var top_div = doc.createElement("div");       top_div.id = '__kx_ad_'+slot;
-                  doc.body.appendChild(top_div);
-                  var top_tag = doc.createElement("script");      top_tag.id = '__kx_top_tag_'+slot;
-                  top_tag.innerHTML = tag.innerHTML;
-                  doc.body.appendChild(top_tag);
                 }
-              }
-            })();
-            </script>
-            <?php }?>
-            <!-- End Pucker Mob - Adhesion - iframe buster code -->
+              })();
+              </script>
+              <!-- End Pucker Mob - Adhesion -->
+
             <?php if( $articleInfoObj['article_id'] == 22475){?>
 
               <!-- SPOUTABLE REPLACE ADHESION -->

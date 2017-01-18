2b27c0578d2f0eec4f2ab87e0a1ae6fc5715d7bd
gbrisset <brissetconsulting@gmail.com>
2017-01-18 11:27:07 -0500

    Kixer restoredNew trial page in  bottomscripts.php

--- httpdocs/assets/includes/bottomscripts.php
+++ httpdocs/assets/includes/bottomscripts.php
@@ -80,13 +80,55 @@
             ?>
             <?php if( $articleInfoObj['article_id'] == 27296){?>
             <!-- TEST PAGE - article #27296 http://www.puckermob.com/moblog/what-time-doesnt-heal-you-have-to-heal-yourself  -->
-                    <!-- /73970039/Z_Testing -->
-                    <div id='div-gpt-ad-1484001734438-0' style='height:1px; width:1px;'>
-                    <script>
-                    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1484001734438-0'); });
+            
+                    <!-- Start Pucker Mob - Adhesion - iframe buster tag -->
+                    <div id='__kx_ad_4915'></div>
+                    <img id='__kx_ad_4915_px' src='%%VIEW_URL_UNESC%%http://cdn.kixer.com/img/transparent.gif?cb=%%CACHEBUSTER%%' />
+                    <script type="text/javascript" language="javascript" id="__kx_tag_4915">
+                    var __kx_ad_slots = __kx_ad_slots || [];
+                    (function () {
+                      var slot = 4915;
+                      var h = false;
+                      var doc = document;
+                      __kx_ad_slots.push(slot);
+                        if (typeof __kx_ad_start == 'function') {
+                        __kx_ad_start();
+                      } else {
+                        if (top == self) {
+                          var s = doc.createElement('script');
+                          s.type = 'text/javascript';
+                          s.async = true;
+                          s.src = '//cdn.kixer.com/ad/load.js';
+                          s.onload = s.onreadystatechange = function(){
+                            if (!h && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+                              h = true;
+                              s.onload = s.onreadystatechange = null;
+                              __kx_ad_start();
+                            }
+                          };
+                          var x = doc.getElementsByTagName('script')[0];
+                          x.parentNode.insertBefore(s, x);
+                        } else {
+                          var tag = doc.getElementById('__kx_tag_'+slot);
+                          var win = window.parent;
+                          doc = win.document;
+                          var top_div = doc.createElement("div");
+                          top_div.id = '__kx_ad_'+slot;
+                          doc.body.appendChild(top_div);
+                          var top_tag = doc.createElement("script");
+                          top_tag.id = '__kx_top_tag_'+slot;
+                          top_tag.innerHTML = tag.innerHTML;
+                          doc.body.appendChild(top_tag);
+                        }
+                      }
+                    })();
                     </script>
-                    </div>
-           <?php  }else{ ?>
+                    <!-- End Pucker Mob - Adhesion - iframe buster code -->
+      
+
+
+            <!-- END OF TEST PAGE - article #27296 http://www.puckermob.com/moblog/what-time-doesnt-heal-you-have-to-heal-yourself  -->
+            <?php  }else{ ?>
 
            <!-- ----- This ad is suspended until Adhesion figures out the test page above -- GB 2017-01-16 ----  -->
                   <!-- /73970039/UT_Adhesion -->

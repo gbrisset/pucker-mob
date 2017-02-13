ccccecd7bd78bc3874a9a47826926d76a6443637
gbrisset <brissetconsulting@gmail.com>
2017-02-13 13:33:36 -0500

    remove video on article #26139

--- httpdocs/article.php
+++ httpdocs/article.php
@@ -306,37 +306,40 @@ if(!is_null($categoryInfo)){
 					<!-- IMAGE -->
 					<div id="article-image" class="small-12 clear">
 
-					<!-- FORCING THE VIDEO INSTEAD THE IMAGE							 -->
-							 <?php if(isset($articleInfoObj['article_id']) &&  $articleInfoObj['article_id'] == 23564){ 
-							 	// #23564 -- http://www.puckermob.com/relationships/dating-pitfalls-avoiding-the-freaks-geeks-and-the-thoroughly-undatable
-							 	// This article is paid content
-							 ?>
+					<!-- FORCING THE VIDEO INSTEAD OF THE IMAGE							 -->
+							 <?php if(isset($articleInfoObj['article_id'])){ 
+								 if($articleInfoObj['article_id'] == 23564 || $articleInfoObj['article_id'] == 26139 ){ 
+								 	// #23564 -- http://www.puckermob.com/relationships/dating-pitfalls-avoiding-the-freaks-geeks-and-the-thoroughly-undatable
+								 	// #26139 -- http://www.puckermob.com/moblog/to-the-20-somethings-looking-for-love-check-out-inner-circle
+								 	// These articles are paid content
+								 ?>
 
-									<img src="<?php echo 'http://images.puckermob.com/articlesites/puckermob/large/'.$article_id.'_tall.jpg'; ?>" alt="<?php echo $article_title; ?> Image"> 
+										<img src="<?php echo 'http://images.puckermob.com/articlesites/puckermob/large/'.$article_id.'_tall.jpg'; ?>" alt="<?php echo $article_title; ?> Image"> 
 
-							 <?php }else{?>
+								 <?php }else{?>
 
-									<div id="vm_player"></div>
-									<script>
-									window._videomosh = window._videomosh || [];
+										<div id="vm_player"></div>
+										<script>
+										window._videomosh = window._videomosh || [];
 
-									!function (e, f, u) {
-									e.async = 1;
-									e.src = u;
-									f.parentNode.insertBefore(e, f);
-									}(document.createElement('script'),
-									document.getElementsByTagName('script')[0],
-									'http://player.videomosh.com/players/loader/loader_final4.js');
+										!function (e, f, u) {
+										e.async = 1;
+										e.src = u;
+										f.parentNode.insertBefore(e, f);
+										}(document.createElement('script'),
+										document.getElementsByTagName('script')[0],
+										'http://player.videomosh.com/players/loader/loader_final4.js');
 
-									_videomosh.push({publisher_key:"sequelmedia",
-														mode: "player",
-														container: "vm_player",
-														target_type: "mix",
-														type:"video",
-														id:231668});
-									</script>
+										_videomosh.push({publisher_key:"sequelmedia",
+															mode: "player",
+															container: "vm_player",
+															target_type: "mix",
+															type:"video",
+															id:231668});
+										</script>
 
-							 <?php }//end if (isset($articleInfoObj['article_id']) ?>
+								 <?php }//end if($articleInfoObj['article_id'] == 23564 ...) ?>
+							 <?php }//end if (isset($articleInfoObj ... ) ?>
 
 
 							<?php

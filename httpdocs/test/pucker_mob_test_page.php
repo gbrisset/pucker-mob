25288ca1470f5bb22b5a85d56dccea8f0ad29c31
gbrisset <brissetconsulting@gmail.com>
2017-09-27 17:16:41 -0400

    New ad manager

--- httpdocs/test/pucker_mob_test_page.php
+++ httpdocs/test/pucker_mob_test_page.php
@@ -27,7 +27,7 @@ class smf_ad_manager extends Connector{
 		$this->config = $c;
 		$this->con = new Connector($this->config);
 	    
-	}//end function __construct()
+	}//end function ------------------------------------------------------------------------------
 
 
 		function get_article_body($article_id){
@@ -43,75 +43,57 @@ class smf_ad_manager extends Connector{
 
 		            $this->closeCon();
 		        return $r;
-		}//end function
+		}//end function ------------------------------------------------------------------------------
 		       
 		function get_article_body_with_ads($article_id,$target_device){
 
+			//body text data
 			$article_data = $this->get_article_body($article_id);
 			$body_text = $article_data['article_body'];
 			$max_pos = strlen($body_text);
+
+			//html tags structuring the body text
+			$searched_html_tags[] = array('tag' => '</p>', 'length' => 4 );
+			$searched_html_tags[] = array('tag' => '</li>', 'length' => 5 );
+			
+			// Ad slots available
 			$inline_ad_slots = $this->get_inline_ad_slots($max_pos, $target_device);
 
+			//Part One - Insertion of the placeholders
+			// ------------------------------------------
 			foreach ($inline_ad_slots as $key => $inline_ad_slot) {
-				$tag_placeholder = 'ADTAG_$key';
-				$offset_search = $key * strlen($tag_placeholder); //0, 9, 18, 27, ...
-				$inline_pos = $inline_ad_slot['inline_pos'];
-//-------------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------
+				$ad_tag_placeholder[$key] = '<!-- #ADTAG_' . $key . '# -->';// placeholders must stay hidden if no ad if found for a given slot
+				$as_inline_pos = $inline_ad_slot['as_inline_pos'];
+				$ad_tag_pos[$key] = $max_pos;
 
-$pos_p1 = 4 + strpos($body_text, '</p>', 500);
-$pos_l1 = 5 + strpos($body_text, '</li>', 500);
-$pos_1 = min(($pos_p1>4? $pos_p1:$max_pos), ($pos_l1>5? $pos_l1:$max_pos));
+				//search for the closest html tag after the desired inline position (assumption is made that THERE ARE <p> or <li> tags structuring the body text)
+				foreach ($searched_html_tags as $html_tag) {
+					$temp_pos = $html_tag['length'] + strpos($body_text, $html_tag['tag'], $as_inline_pos);
+					 if($temp_pos > $html_tag['length'] ) $ad_tag_pos[$key] = $temp_pos; 
+				}//end foreach ($searched_html_tags
 
-$pos_p2 = 4 + 10 + strpos($body_text, '</p>', 1300);
-$pos_l2 = 5 + 10 + strpos($body_text, '</li>', 1300);
-$pos_2 = min(($pos_p2>14? $pos_p2:$max_pos), ($pos_l2>15? $pos_l2:$max_pos));
+				$body_text = substr_replace($body_text, $ad_tag_placeholder[$key], $ad_tag_pos[$key], 0);
 
-$pos_p3 = 4 + 20 + strpos($body_text, '</p>', 2100);
-$pos_l3 = 5 + 20 + strpos($body_text, '</li>', 2100);
-$pos_3 = min(($pos_p3>24? $pos_p3:$max_pos), ($pos_l3>25? $pos_l3:$max_pos));
+			}//end foreach ($inline_ad_slots ...
 
-$pos_p4 = 4 + 30 + strpos($body_text, '</p>', 2900);
-$pos_l4 = 5 + 30 + strpos($body_text, '</li>', 2900);
-$pos_4 = min(($pos_p4>34? $pos_p4:$max_pos), ($pos_l4>35? $pos_l4:$max_pos));
+			//Part Two - Insertion of the ad tags
+			// ------------------------------------------
+			reset($inline_ad_slots);
+			foreach ($inline_ad_slots as $key => $inline_ad_slot) {
 
-$body_text = substr_replace($body_text, '#ADTAG_01#', $pos_1, 0);
-$body_text = substr_replace($body_text, '#ADTAG_02#', $pos_2, 0);
-$body_text = substr_replace($body_text, '#ADTAG_03#', $pos_3, 0);
-$body_text = substr_replace($body_text, '#ADTAG_04#', $pos_4, 0);
+				$inline_ad_tag = $this->get_adtag($article_id, $inline_ad_slot['as_name']);
+				$body_text = str_replace($ad_tag_placeholder[$key], $inline_ad_tag['at_script'], $body_text);
 
-$body_text = str_replace('#ADTAG_01#', $sample_tag, $body_text);
-$body_text = str_replace('#ADTAG_02#', $sample_tag, $body_text);
-$body_text = str_replace('#ADTAG_03#', $sample_tag, $body_text);
-$body_text = str_replace('#ADTAG_04#', $sample_tag, $body_text);
+			}//end foreach ($inline_ad_slots ...
 
+			return $body_text;
 
-//-------------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------
-
-
-			}//end foreach
-
-
-// echo "<pre>";
-// var_dump($inline_ad_slots);
-// echo "</pre>";
-// echo "$max_pos";
-
-		}//end function
+		}//end function ------------------------------------------------------------------------------
 		       
 		function get_inline_ad_slots($max_pos, $target_device){
 			$pdo = $this->con->openCon();        
 
 		        $querystring = "SELECT * FROM `smf_ad_slots` WHERE as_inline_pos >0 AND as_inline_pos < $max_pos AND as_target_device = $target_device;  ";
-		        echo "$querystring";
 		            $q = $pdo->query($querystring);
 		            if($q && $q->rowCount()){
 		                $q->setFetchMode(PDO::FETCH_ASSOC); 
@@ -122,7 +104,7 @@ $body_text = str_replace('#ADTAG_04#', $sample_tag, $body_text);
 
 		            $this->closeCon();
 		        return $r;
-		}//end function
+		}//end function ------------------------------------------------------------------------------
 		       
 		function get_adtag($article_id, $adslot_name){
 
@@ -170,7 +152,29 @@ $body_text = str_replace('#ADTAG_04#', $sample_tag, $body_text);
 
 		            $this->closeCon();
 		        return $r;
-		}//end function
+		}//end function ------------------------------------------------------------------------------
+
+		function get_all_adtags(){
+
+				// Gets all tags, their adslots and their special pages
+		        $pdo = $this->con->openCon();
+		        $querystring = "
+					SELECT * FROM smf_ad_tags smf_at
+					INNER JOIN smf_ad_tag_to_ad_slot atas ON atas.atas_ad_tag_id = smf_at.at_id
+					INNER JOIN smf_ad_slots smf_as ON smf_as.as_id = atas.atas_ad_slot_id
+		         "; 
+		         echo "$querystring";//TEST echo "$querystring";//TEST echo "$querystring";//TEST echo "$querystring";//TEST echo "$querystring";//TEST echo "$querystring";//TEST echo "$querystring";//TEST echo "$querystring";//TEST 
+		            $q = $pdo->query($querystring);
+		            if($q && $q->rowCount()){
+		                $q->setFetchMode(PDO::FETCH_ASSOC); 
+		                $row = $q->fetchAll();
+		                $r = $row;
+		                $q->closeCursor();
+		            }else $r = false;
+
+		            $this->closeCon();
+		        return $r;
+		}//end function ------------------------------------------------------------------------------
 
 }//end class smf_ad_manager extends Connector
 
@@ -188,12 +192,16 @@ $body_text = str_replace('#ADTAG_04#', $sample_tag, $body_text);
 <?php
 
 
+//CONFIG LEVEL
 $smf_ad_manager = new smf_ad_manager($config);
 
+
+// ARTICLE LEVEL
 // $article_id = 8560; //lelo
 // $article_id = 27328;
-$article_id = 8541; //rubicon
-// $article_id = 11111;
+// $article_id = 8541; //rubicon
+// $article_id = 11111;		// $ddd = new debug("$s",0); $ddd->show();// 0- green; 1-red; 2-grey; 3-yellow	
+
 
 $target_device = 1; //0 = dsk, 1 = mbl
 
@@ -202,66 +210,22 @@ $target_device = 1; //0 = dsk, 1 = mbl
 // $adslot_name = 'mbl_body_text_1';
 // $adslot_name = 'mbl_body_text_2';
 // $adslot_name = 'mbl_ad_stack_1';
-$adslot_name = 'mbl_ad_stack_2';
+// $adslot_name = 'mbl_ad_stack_2';
 
-$ad_to_display = $smf_ad_manager->get_adtag($article_id, $adslot_name);
+// Call for a specific ad slot
+// $ad_to_display = $smf_ad_manager->get_adtag($article_id, $adslot_name);
 
-
-
-// $sample_tag = "<div style=\"height: 250px; width: 300px; margin:20px 0; text-align: center;\">" . $sample_tag . "</div>";
-// $sample_article = get_article_body($article_id);
-
-$body_text_with_ads = $smf_ad_manager->get_article_body_with_ads($article_id,$target_device);
-
+// Call For inline_ad_slots
+// $body_text_with_ads = $smf_ad_manager->get_article_body_with_ads($article_id,$target_device);
 // echo $body_text_with_ads;
 
-exit();
+
+//ADMIN LEVEL
 
 
-/* ******************************************************************************* */
-/* ******************************************************************************* */
-/* ******************************************************************************* */
-/* ******************************************************************************* */
-/* ******************************************************************************* */
-/* ******************************************************************************* */
+$all_adtags = $smf_ad_manager->get_all_adtags();
 
-
-// $body_text = $sample_article['article_body'];
-// $max_pos = strlen($body_text);
-// //#ADTAG_01# is 10 chars long
-// //</p> is 4 chars long
-// //</li> is 5 chars long
-
-// $pos_p1 = 4 + strpos($body_text, '</p>', 500);
-// $pos_l1 = 5 + strpos($body_text, '</li>', 500);
-// $pos_1 = min(($pos_p1>4? $pos_p1:$max_pos), ($pos_l1>5? $pos_l1:$max_pos));
-
-// $pos_p2 = 4 + 10 + strpos($body_text, '</p>', 1300);
-// $pos_l2 = 5 + 10 + strpos($body_text, '</li>', 1300);
-// $pos_2 = min(($pos_p2>14? $pos_p2:$max_pos), ($pos_l2>15? $pos_l2:$max_pos));
-
-// $pos_p3 = 4 + 20 + strpos($body_text, '</p>', 2100);
-// $pos_l3 = 5 + 20 + strpos($body_text, '</li>', 2100);
-// $pos_3 = min(($pos_p3>24? $pos_p3:$max_pos), ($pos_l3>25? $pos_l3:$max_pos));
-
-// $pos_p4 = 4 + 30 + strpos($body_text, '</p>', 2900);
-// $pos_l4 = 5 + 30 + strpos($body_text, '</li>', 2900);
-// $pos_4 = min(($pos_p4>34? $pos_p4:$max_pos), ($pos_l4>35? $pos_l4:$max_pos));
-
-// $body_text = substr_replace($body_text, '#ADTAG_01#', $pos_1, 0);
-// $body_text = substr_replace($body_text, '#ADTAG_02#', $pos_2, 0);
-// $body_text = substr_replace($body_text, '#ADTAG_03#', $pos_3, 0);
-// $body_text = substr_replace($body_text, '#ADTAG_04#', $pos_4, 0);
-
-// $body_text = str_replace('#ADTAG_01#', $sample_tag, $body_text);
-// $body_text = str_replace('#ADTAG_02#', $sample_tag, $body_text);
-// $body_text = str_replace('#ADTAG_03#', $sample_tag, $body_text);
-// $body_text = str_replace('#ADTAG_04#', $sample_tag, $body_text);
-
- // $ddd = new debug("$body_text",3); $ddd->show();// 0- green; 1-red; 2-grey; 3-yellow  
-// echo "<pre>";
-// var_dump($sample_article);
-// echo "</pre>";
+		$ddd = new debug($all_adtags,3); $ddd->show();// 0- green; 1-red; 2-grey; 3-yellow	
 
 ?>
 

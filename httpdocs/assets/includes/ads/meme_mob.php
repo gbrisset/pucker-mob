e61fe60ebb1e64552c3748944bf95f303a3eb87f
gbrisset <brissetconsulting@gmail.com>
2017-10-10 17:20:01 -0400

    Ad Update

---
+++ httpdocs/assets/includes/ads/meme_mob.php
@@ -0,0 +1,16 @@
+<!-- meme mob -->
+
+<script>
+  googletag.cmd.push(function() {
+    googletag.defineSlot('/73970039/300x250_ROS', [300, 250], 'div-gpt-ad-1507665588788-0').addService(googletag.pubads());
+    googletag.pubads().enableSingleRequest();
+    googletag.enableServices();
+  });
+</script>
+
+<!-- /73970039/300x250_ROS -->
+<div id='div-gpt-ad-1507665588788-0' style='height:250px; width:300px;    margin: 15px auto;'>
+<script>
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1507665588788-0'); });
+</script>
+</div>
